af4398c9ff41f4a224ab5d797e27fc3c0f227609
mokom <mokomgeh45@gmail.com>
2022-11-22 14:40:54 -0500

    updated test forvalid tests

--- app/blog/tests.py
+++ app/blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        test_user = User.objects.create_user(username='testuser', password='12345')
+        Post.objects.create(title="django", slug="django", author=test_user, status="published")
+
+    def test_failure(self):
+        qs = Post.objects.all()
+        self.assertTrue(qs.exists())
